dfb2c6cdf598374ba8c5091f1691c8b2a2c309c1
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-22 14:46:48 -0400

    made second schema

--- models/anime.js
+++ models/anime.js
@@ -58,7 +58,7 @@ let AnimeSchema = mongoose.Schema({
  * NOTE: skip this if you are not using mongoose
  *
  */const animeCollection = mongoose.model('anime', AnimeSchema)
- const authorCollection = mongoose.model('anime',AuthorSchema)
+ const AuthorCollection = mongoose.model('anime',AuthorSchema)
 
 /* Step 4
  *
@@ -74,13 +74,38 @@ const CreateAnime = () =>
       })
 
 function getAllAnime() {
-        return IssueCollection.find()
+        return animeCollection.find()
       }
-//  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      const CreateAuthor = () =>
+  AuthorCollection.create({ name: "One Punch Man", created: new Date(), genre: "Harem ", StartDate: new Date(),
+  EndDate:new Date(), mainCharacter:"Saitama" , type:"shounen", creator:"mikashi" 
+      })
+
+function getAllAuthor() {
+    return AuthorCollection.find()
+  }
+
 //  * TODO: export all functions from this file by adding their names as keys to this
 //  * object
 //  */
 module.exports = {
   CreateAnime,
-  getAllAnime
+  getAllAnime,
+  CreateAuthor,
+  getAllAuthor
 }
