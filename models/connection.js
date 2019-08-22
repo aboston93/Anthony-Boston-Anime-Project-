c8b31d58118d6d29357ebecd746125273178acb0
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-22 09:35:23 -0400

    all dat

--- models/connection.js
+++ models/connection.js
@@ -18,11 +18,20 @@ const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-nam
  *
  * NOTE: newUrlParser diables a deprecation warning
  */
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
-
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/<db-name>');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 /* Export the mongoose object.
  *
